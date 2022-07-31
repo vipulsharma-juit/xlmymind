4dc7f86241e33d94e4aa2a66f126578da5d9f33f
Mayank Kumar <211317@juitsolan.in>
2022-07-31 23:18:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,89 +1,69 @@
-import React,{ useEffect } from "react";
-import './App.css';
-import {BrowserRouter as Router, Switch, Route }from "react-router-dom";
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useStateValue } from "./Pages/StateProvider";
-import {auth} from "./firebase"
-import HomePage from './Pages/Homepage/Homepage'
-import Loginpage from './Pages/Loginpage/Loginpage'
-import Registerpage from './Pages/RegisterPage/RegisterPage'
-import ProductsPage from './Pages/ProductsPage/ProductsPage'
-import Cart from './Pages/Cart/Cart'
+import { auth } from "./firebase";
+import HomePage from "./Pages/Homepage/Homepage";
+import Loginpage from "./Pages/Loginpage/Loginpage";
+import Registerpage from "./Pages/RegisterPage/RegisterPage";
+import ProductsPage from "./Pages/ProductsPage/ProductsPage";
+import Cart from "./Pages/Cart/Cart";
 import Checkout from "./Checkout";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import Events from "./Pages/components/Events/Events";
+import EventsPage from "./Pages/Events/EventsPage";
 
 function App() {
+  const [{}, dispathch] = useStateValue();
 
-  const [{},dispathch]=useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
 
-  useEffect(()=>{
-
-        auth.onAuthStateChanged(authUser=>{
-          console.log('THE USER IS >>>',authUser);
-
-          if(authUser)
-          {
-
-            dispatchEvent({
-              type:'SET_USER',
-              user: authUser
-
-            })
-
-          }
-          else
-          {
-
-            dispatchEvent({
-              type:'SET_USER',
-              user: null
-              
-            })
-
-          }
-        })
-
-  },[])
+      if (authUser) {
+        dispatchEvent({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatchEvent({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
-
     <Router>
-    <div className="app">
-   
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Loginpage />
+          </Route>
 
-    
-    <Switch>
-      <Route path="/login">
-        
-        <Loginpage/>
-
-      </Route>
-      
-
-      <Route path="/register">
-        
-        <Registerpage/>
-
-      </Route>
-      <Route path="/products">
-        
-        <ProductsPage/>
-
-      </Route>
+          <Route path="/register">
+            <Registerpage />
+          </Route>
+          <Route path="/products">
+            <ProductsPage />
+          </Route>
+          <Route path="/about">
+            <AboutUs />
+          </Route>
+          <Route path="/events">
+            <EventsPage />
+          </Route>
 
 
-  
-    <Route path="/cart">
-      <Checkout/>
-   
- 
-    
-    </Route>
- 
-    <Route path="/">
-      <HomePage/>
-      </Route>
-    </Switch>
-  
-    
-    </div>
+          <Route path="/cart">
+            <Checkout />
+          </Route>
+
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
