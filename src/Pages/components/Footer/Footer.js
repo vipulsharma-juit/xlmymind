0d14718ea18075bf48ef69769ae51ce959b47bda
vipulsharma-juit <104648176+vipulsharma-juit@users.noreply.github.com>
2022-08-02 23:12:41 +0530

    Update Footer.js

--- src/Pages/components/Footer/Footer.js
+++ src/Pages/components/Footer/Footer.js
@@ -19,8 +19,8 @@ function Footer() {
         <div className='footer-section'>
         <div className='footer-section-header'>Get in Touch</div>
             <div className='section-items'>
-                {/* <div className='section-item'><FontAwesomeIcon icon="envelope"/> xlmymind@gmail.com</div>
-                <div className='section-item'><FontAwesomeIcon icon="phone"/> 9717830336</div> */}
+                <div className='section-item'><FontAwesomeIcon icon="envelope"/> xlmymind@gmail.com</div>
+                <div className='section-item'><FontAwesomeIcon icon="phone"/> 9717830336</div>
             </div>
         </div>
         <div className='footer-section'>
